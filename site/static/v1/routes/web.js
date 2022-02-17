386f2eb9f2121f4d03fea5e156793babdb5a4adb
ahmed8765 <57402517+ahmed8765@users.noreply.github.com>
2022-02-17 17:22:16 +0500

    modify

--- site/static/v1/routes/web.js
+++ site/static/v1/routes/web.js
@@ -223,7 +223,7 @@ function enforce_maxlength(event) {
   document.body.addEventListener('input', enforce_maxlength);
 
 
-  function whatsapp(id,name)
+function whatsapp(id,name)
 {
   $('#whatsapp'+id).attr('href','https://api.whatsapp.com/send/?phone='+social_mobile+'&text='+window.location.origin+'/products/'+id+'%0D%0AProduct Name: *'+name+'*%0D%0ACould you please tell me the availibilty of this product?');
 }
